6d19be043dcdb8a41504cc053ab91bbcc5f9028a
zhangzhewei <zhangzhewei@kuaishou.com>
2019-03-22 19:15:08 +0800

    编码

--- xxl-conf-admin/src/main/java/com/xxl/conf/admin/controller/interceptor/WebMvcConfig.java
+++ xxl-conf-admin/src/main/java/com/xxl/conf/admin/controller/interceptor/WebMvcConfig.java
@@ -1,10 +1,17 @@
 package com.xxl.conf.admin.controller.interceptor;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import java.nio.charset.Charset;
+import java.util.List;
 
 import javax.annotation.Resource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * web mvc config
@@ -29,4 +36,23 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         super.addInterceptors(registry);
     }
 
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        StringHttpMessageConverter converter = new StringHttpMessageConverter(
+                Charset.forName("UTF-8"));
+        return converter;
+    }
+
+    @Override
+    public void configureMessageConverters(
+            List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        converters.add(responseBodyConverter());
+    }
+
+    @Override
+    public void configureContentNegotiation(
+            ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false);
+    }
 }
